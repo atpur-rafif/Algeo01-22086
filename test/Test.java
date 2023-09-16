ef49ee569e114373e17d756392508d5098a1b331
InTheRain <indrswara@gmail.com>
2023-09-16 20:52:49 +0700

    Implemented matrix arithmetic

--- test/Test.java
+++ test/Test.java
@@ -3,12 +3,19 @@ import Matrix.*;
 public class Test {
     public static void main(String[] args){
         var M = MatrixReader.read();
-        MatrixPrinter.print(M);
 
-        var Manipulator = new MatrixManipulator(M);
-        Manipulator.gausJordanElimination();
+        var m = MatrixReader.read();
 
-        System.out.println("");
-        MatrixPrinter.print(Manipulator.getResult());
+        var result = MatrixArithmetic.Multiply(M, m);
+        
+
+        
+        MatrixPrinter.print(result);
+
+        // var Manipulator = new MatrixManipulator(M);
+        // Manipulator.gausJordanElimination();
+
+        // System.out.println("");
+        // MatrixPrinter.print(Manipulator.getResult());
     }
 }
