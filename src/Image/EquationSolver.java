79632f05715e203bd006fadadfc10d8577884181
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-09-17 18:12:28 +0700

    Implemented flatten from 2D to 1D array

---
+++ src/Image/EquationSolver.java
@@ -0,0 +1,18 @@
+package Image;
+
+import Point.Point;
+
+public class EquationSolver {
+    static Point[] points = {
+        new Point(0, 0),
+        new Point(0, 1),
+        new Point(1, 0),
+        new Point(1, 1)
+    };
+
+    static int sampleSideCount = 4;
+    static int coordinateOffset = (-1) * sampleSideCount + (-1);
+    public static int flattenCoordinate(int x, int y){
+        return (y * sampleSideCount) + x - coordinateOffset;
+    }
+}
--- test/Test.java
+++ test/Test.java
@@ -1,6 +1,7 @@
 import Matrix.*;
 
 import Application.*;
+import Image.EquationSolver;
 import Image.ImageLoader;
 import Image.ImageSaver;
 
@@ -11,9 +12,11 @@ import javax.imageio.ImageIO;
 
 public class Test {
     public static void main(String[] args){
-        var g = ImageLoader.load("./test/Image.jpeg");
-        System.out.print(g.height);
-
-        ImageSaver.save(g, "./tmp/Image.jpeg");
+        int i, j;
+        for(i = -1; i <= 2; ++i){
+            for(j = -1; j <= 2; ++j){
+                System.out.println(j + "," + i + ": " + EquationSolver.flattenCoordinate(j, i));
+            }
+        }
     }
 }
