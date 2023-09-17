0d776acf16a36c163c001ef3981396c3a4c46795
InTheRain <indra@DESKTOP-4H8LC8L>
2023-09-17 10:56:27 +0700

    add determinant class

--- test/Test.java
+++ test/Test.java
@@ -4,13 +4,12 @@ public class Test {
     public static void main(String[] args){
         var M = MatrixReader.read();
 
-        var m = MatrixReader.read();
 
-        var result = MatrixArithmetic.Multiply(M, m);
-        
+        var result = MatrixDeterminant.calculate(M);
+
 
         
-        MatrixPrinter.print(result);
+        System.out.println(result);
 
         // var Manipulator = new MatrixManipulator(M);
         // Manipulator.gausJordanElimination();
