d6e821ef19978df4d5f31b74d49f131416a9c218
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-09-21 15:01:59 +0700

    Move LocalSplineTransformation

--- src/Vector/EquationSpace.java
+++ src/Vector/EquationSpace.java
@@ -1,7 +1,9 @@
 package Vector;
 
 public class EquationSpace extends VectorSpace{
+    public int independentVariableCount = 0;
     EquationSpace(int indepentdentVariableCount){
         super(indepentdentVariableCount);
+        this.independentVariableCount = indepentdentVariableCount;
     }
 }
