9ebc7b3ab03730966cf2189aaaa9eac85af083fd
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-09-21 15:17:14 +0700

    Removed unused file

--- test/Test.java
+++ test/Test.java
@@ -9,7 +9,7 @@ import Image.*;
 public class Test {
     public static void main(String[] args){
         var image = ImageLoader.load("./tmp/debug.png");
-        var resized = Resize.resize(image, 1.5);
+        var resized = Resize.resize(image, 10);
         ImageSaver.save(resized, "./tmp/debug-res.png");
     }
 }
