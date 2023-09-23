7e30463984f05b60571b8594392698bbc518c07e
InTheRain <indrswara@gmail.com>
2023-09-23 18:20:10 +0700

    Add ListProgram (changeable anytime)

--- src/Menu/MainMenu.java
+++ src/Menu/MainMenu.java
@@ -10,15 +10,28 @@ public class MainMenu {
         System.out.println("================Halo Selamat Datang==================");
         System.out.println("=====================================================");
         System.out.println("\n\n");
+        System.out.println("===================List Program======================");
+        ListProgram();
         System.out.println("Masukkan Input: ");
-        System.out.println("> ");
+        System.out.print("> ");
         int choice = Integer.parseInt(scanner.next());
-        while (choice > 1 && choice < 10) {
+        while (choice >= 1 && choice < 9) {
             System.out.println("test");
             choice = Integer.parseInt(scanner.next());
+
         }
 
+    }
 
+    private static void ListProgram(){
+        System.out.println("1. Gauss Elimination");
+        System.out.println("2. GaussJordan Elimination");
+        System.out.println("3. Inverse Matrix");
+        System.out.println("4. Determinant");
+        System.out.println("5. Interpolinom");
+        System.out.println("6. Multiple Linear");
+        System.out.println("7. Bicubic Spline");
+        System.out.println("8. Quit");
 
     }
 }
