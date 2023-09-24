a70c4cac1b01fb1a3742cbaa51bfe1e22ae0403f
InTheRain <indrswara@gmail.com>
2023-09-24 16:03:59 +0700

    add matrix reader from file

--- src/Matrix/MatrixReader.java
+++ src/Matrix/MatrixReader.java
@@ -1,26 +1,58 @@
 package Matrix;
 
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class MatrixReader {
     static Scanner scanner = new Scanner(System.in);
-
+    static BufferedReader file;
+    
     public static Matrix read(){
         System.out.print("Masukkan baris: ");
         int row = Integer.parseInt(scanner.next());
-
+        
         System.out.print("Masukkan kolom: ");
         int col = Integer.parseInt(scanner.next());
-
+        
         return MatrixReader.read(row, col);
     }
-
+    
     public static Matrix read(int row, int col){
         var M = new Matrix(row, col);
 
         for(int i = 0; i < row; ++i){
             for(int j = 0; j < col; ++j){
                 M.set(i, j, Double.parseDouble(scanner.next()));
+            }
+        }
+        return M;
+    }
+
+    public static Matrix readFile(String fileName){
+        var M = new Matrix(0, 0);
+        boolean isReadFileSucceed = false; 
+        while(!isReadFileSucceed) {
+            try {
+                file = new BufferedReader(new FileReader(fileName));
+                var fileScanner = new Scanner(file);
+                int row = Integer.parseInt(fileScanner.next());
+                int col = Integer.parseInt(fileScanner.next());
+                M = new Matrix(row, col); 
+        
+                for(int i = 0; i < row; ++i){
+                    for(int j = 0; j < col; ++j){
+                        M.set(i, j, Double.parseDouble(fileScanner.next()));
+                    }
+                }
+                isReadFileSucceed = true;
+                fileScanner.close();
+            }
+            catch(IOException wrong) {
+                System.out.println("Cant Read File");
+
             }
         }
 
--- test/Test.java
+++ test/Test.java
@@ -3,14 +3,8 @@ import Matrix.OBE.OBERunner;
 
 public class Test {
     public static void main(String[] args){
-        var m = MatrixReader.read();
-        var r = new OBERunner(m);
-        r.gausJordanElimination();
-        MatrixPrinter.print(r.getResult());
-
-        var k = r.getLogs();
-        for(int i = 0; i < k.length; ++i){
-            System.out.println(k[i]);
-        }
+        
+        Matrix testing = MatrixReader.readFile("./test/test.txt"); 
+        MatrixPrinter.print(testing);
     }
 }
