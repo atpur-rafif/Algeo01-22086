8879cee972467e070804efbc1537a7f06fa005ff
InTheRain <indrswara@gmail.com>
2023-09-24 11:56:01 +0700

    Menu update

--- src/Menu/MainMenu.java
+++ src/Menu/MainMenu.java
@@ -1,7 +1,8 @@
 package Menu;
 
 import java.util.Scanner;
-
+import java.lang.Runtime;
+import Matrix.*;
 
 public class MainMenu {
     static Scanner scanner = new Scanner(System.in);
@@ -9,29 +10,30 @@ public class MainMenu {
     public static void InterfaceProgram(){
         System.out.println("================Halo Selamat Datang==================");
         System.out.println("=====================================================");
-        System.out.println("\n\n");
-        System.out.println("===================List Program======================");
+        System.out.println("\n");
         ListProgram();
         System.out.println("Masukkan Input: ");
         System.out.print("> ");
         int choice = Integer.parseInt(scanner.next());
         while (choice >= 1 && choice < 9) {
             System.out.println("test");
+            ListProgram();
             choice = Integer.parseInt(scanner.next());
-
+            
         }
 
     }
 
     private static void ListProgram(){
-        System.out.println("1. Gauss Elimination");
-        System.out.println("2. GaussJordan Elimination");
-        System.out.println("3. Inverse Matrix");
-        System.out.println("4. Determinant");
-        System.out.println("5. Interpolinom");
-        System.out.println("6. Multiple Linear");
-        System.out.println("7. Bicubic Spline");
-        System.out.println("8. Quit");
+        System.out.println("===================List Program======================");
+        System.out.println("1. Gaussian Elimination");
+        System.out.println("2. Inverse Matrix");
+        System.out.println("3. Determinant");
+        System.out.println("4. Interpolinom");
+        System.out.println("5. Multiple Linear");
+        System.out.println("6. Bicubic Spline");
+        System.out.println("7. Quit");
 
     }
+
 }
