0ce520499b1d54664a522df989e5684bc119043e
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-09-25 07:19:00 +0700

    Made vector space generic

--- src/Vector/VectorSpace.java
+++ src/Vector/VectorSpace.java
@@ -1,6 +1,6 @@
 package Vector;
 
-public class VectorSpace {
+public class VectorSpace{
     public int basisCount;
     double[] data;
 
@@ -18,22 +18,32 @@ public class VectorSpace {
         this.data[i] = newValue;
     }
 
-    public static VectorSpace add(VectorSpace v1, VectorSpace v2){
+    @SuppressWarnings("unchecked")
+    private static <T extends VectorSpace> T createBase(T t){
+        try {
+            var clazz = t.getClass();
+            t = (T) clazz.getDeclaredConstructor(int.class).newInstance(t.basisCount);
+        } catch (Exception e) {
+        }
+        return t;
+    }
+
+    public static <T extends VectorSpace> T add(T v1, T v2){
         if(v1.basisCount != v2.basisCount) throw new Error("Basis count should be equal");
-        var r = new VectorSpace(v1.basisCount);
-        for(int i = 0; i < v1.basisCount; ++i){
+        var r = createBase(v1);
+        for (int i = 0; i < v1.basisCount; ++i) {
             r.set(i, v1.get(i) + v2.get(i));
         }
+        return v1;
+    }
+
+    public static <T extends VectorSpace> T scale(T v, double s){
+        var r = createBase(v);
+        for(int i = 0; i < v.basisCount; ++i) r.set(i, v.get(i) * s);
         return r;
     }
 
-    public static VectorSpace scale(VectorSpace v1, double s){
-        var r = new VectorSpace(v1.basisCount);
-        for(int i = 0; i < v1.basisCount; ++i) r.set(i, v1.get(i) * s);
-        return r;
-    }
-
-    public static double innerProduct(VectorSpace v1, VectorSpace v2){
+    public static <T extends VectorSpace> double innerProduct(T v1, T v2){
         if(v1.basisCount != v2.basisCount) throw new Error("Basis count should be equal");
         double r = 0;
         for(int i = 0; i < v1.basisCount; ++i) r += v1.get(i) * v2.get(i);
--- test/Test.java
+++ test/Test.java
@@ -1,10 +1,15 @@
 import Matrix.*;
 import Matrix.OBE.OBERunner;
+import Vector.EuclideanSpace;
+import Vector.VectorSpace;
 
 public class Test {
     public static void main(String[] args){
-        
-        Matrix testing = MatrixReader.readFileCLI();
-        MatrixPrinter.print(testing);
+        var a = new EuclideanSpace(1);
+        var b = new EuclideanSpace(1);
+        a.set(0, 10);
+
+        var c = VectorSpace.scale(a, 10);
+        System.out.println(c.get(0));
     }
 }
