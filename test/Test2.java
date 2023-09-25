37f0d161b0f4eb69b1a289937814e60ed6dd63a2
HaniefFatkhan <hanieffatkhan@gmail.com>
2023-09-25 20:15:25 +0700

    Added Polynomial Interpolation

--- test/Test2.java
+++ test/Test2.java
@@ -8,9 +8,20 @@ import Image.*;
 
 public class Test2 {
     public static void main(String[] args){
-        var M = MatrixReader.read();
-        var O = new OBERunner(M);
-        O.gaussJordanElimination_v2();
-        MatrixPrinter.print(O.getResult());
+        double[][] arr;
+        arr = new double[3][2];
+        arr[0][0] = 8;
+        arr[0][1] = 2.0794;
+        arr[1][0] = 9;
+        arr[1][1] = 2.1972;
+        arr[2][0] = 9.5;
+        arr[2][1] = 2.2512;
+
+        var solution = PolynomialInterp.solve(arr);
+        
+        for (int i=0;i<solution.length;i++){
+            System.out.println(solution[i]);
+        }
+
     }
 }
