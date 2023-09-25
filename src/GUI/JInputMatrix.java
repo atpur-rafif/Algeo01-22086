53ace432dc5a17f12673428b4daeafc2f0863701
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-09-26 00:57:33 +0700

    Text area input box

---
+++ src/GUI/JInputMatrix.java
@@ -0,0 +1,75 @@
+package GUI;
+
+
+import javax.swing.*;
+import javax.swing.event.*;
+
+import Matrix.MatrixPrinter;
+import Matrix.MatrixReader;
+
+import java.awt.*;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class JInputMatrix extends JPanel{
+    public JTextArea textArea = new JTextArea(10, 30);
+    public JLabel output = new JLabel("Output");
+    static int resetCounter = 0;
+
+    void parseAndShowResult(){
+        try {
+            var s = new Scanner(textArea.getText());
+            var m = MatrixReader.read(s);
+            MatrixPrinter.print(m);
+            output.setForeground(Color.BLACK);
+            output.setText(m.toString());
+        } catch (Exception err) {
+            String msg = "";
+            if (err instanceof NumberFormatException) {
+                String cause = err.getMessage().replace("For input string: ", "");
+                msg = "Invalid string: " + cause ;
+            } else if (err instanceof NoSuchElementException) {
+                msg = "Invalid format";
+            } else {
+                msg = "Unknown error";
+            }
+            output.setForeground(Color.RED);
+            output.setText(msg);
+        }
+    }
+
+    JInputMatrix(){
+        var titleLabel = new JLabel("Matrix Input");
+
+        var layout = new GroupLayout(this);
+        this.setLayout(layout);
+
+        textArea.getDocument().addDocumentListener(new DocumentListener() {
+			@Override
+			public void insertUpdate(DocumentEvent e) { update(e); }
+			@Override
+			public void removeUpdate(DocumentEvent e) { update(e); }
+			@Override
+			public void changedUpdate(DocumentEvent e) { update(e); }
+
+            protected void update(DocumentEvent e){
+                parseAndShowResult();
+                SwingUtilities.getWindowAncestor(textArea).pack();
+            }
+        });
+
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.CENTER)
+                .addComponent(titleLabel)
+                .addComponent(textArea)
+                .addComponent(output)
+        );
+
+        layout.setVerticalGroup(
+            layout.createSequentialGroup()
+                .addComponent(titleLabel)
+                .addComponent(textArea)
+                .addComponent(output)
+        );
+    }
+}
--- src/GUI/MainApp.java
+++ src/GUI/MainApp.java
@@ -2,54 +2,16 @@ package GUI;
 
 import javax.swing.*;
 
-import java.awt.event.*;
-
 public class MainApp{
-    static int resetCounter = 0;
     public static void Show(){
         var frame = new JFrame("Matrix");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        var label = new JLabel("Reset Counter: 0");
-        var button = new JButton("Reset");
-        var textArea = new JTextArea(10, 10);
-
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                resetCounter += 1;
-                label.setText("Reset Counter: " + resetCounter);
-                textArea.setText("");
-                frame.pack();
-            }
-        });
-
-        var panel = frame.getContentPane();
-        var layout = new GroupLayout(panel);
-        panel.setLayout(layout);
-
-        layout.setAutoCreateGaps(true);
-        layout.setAutoCreateContainerGaps(true);
-
-        layout.setHorizontalGroup(
-            layout.createParallelGroup()
-                .addGroup(layout.createSequentialGroup()
-                    .addComponent(label)
-                    .addComponent(button)
-                )
-                .addComponent(textArea)
-        );
-
-        layout.setVerticalGroup(
-            layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                    .addComponent(label)
-                    .addComponent(button)
-                )
-                .addComponent(textArea)
-        );
+        var matrixInput = new JInputMatrix();
+        frame.getContentPane().add(matrixInput);
 
         frame.pack();
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 
