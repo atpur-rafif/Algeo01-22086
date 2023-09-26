95a65b25fd92743c6a2b3a1c102d063fd2c27e29
Hanief Nashrullah <68178470+hannoobz@users.noreply.github.com>
2023-09-26 07:52:58 +0700

    update output format

--- src/Menu/InterpolinomMenu.java
+++ src/Menu/InterpolinomMenu.java
@@ -19,18 +19,39 @@ public class InterpolinomMenu {
                 
                 //CLI
                 case "1": 
-                    var Matriks = MatrixReader.readCLI();
+                    System.out.print("Masukkan Jumlah Titik : ");
+                    int n = Integer.parseInt(scanner.next());
+                    var Matriks = MatrixReader.readCLI(n,2);
+                    var interpolatedFunc = PolynomialInterp.calculate(Matriks);
+                    System.out.print("f(x) = ");
+                    for (int i=n-1;i>=0;i--){
+                        System.out.print("("+interpolatedFunc.get(i, 0)+")x^"+i);
+                        if(i!=0){
+                            System.out.print("+");
+                        }
+                    }
+                    System.out.print("\nx = ");
                     double x = Double.parseDouble(scanner.next());
-                    double result= PolynomialInterp.f(Matriks, x);
-                    System.out.println("Hasil: "+ result);
+                    double fxresult= PolynomialInterp.f(Matriks, x);
+                    System.out.println("f("+x+") = "+ fxresult);
 
 
                     break;
                 //FILE
                 case "2": 
                     var MatriksFile = MatrixReader.readFileCLI();
+                    var interpolatedFuncFile = PolynomialInterp.calculate(MatriksFile);
+                    System.out.print("f(x) = ");
+                    for (int i=MatriksFile.row-1;i>=0;i--){
+                        System.out.print("("+interpolatedFuncFile.get(i, 0)+")x^"+i);
+                        if(i!=0){
+                            System.out.print("+");
+                        }
+                    }
+                    System.out.print("\nx = ");
                     double xFile = Double.parseDouble(scanner.next());
                     double resultFile= PolynomialInterp.f(MatriksFile, xFile);
+                    System.out.println("f("+xFile+") = "+ resultFile);
                     System.out.println("Hasil: "+ resultFile);
 
                     break; 
