97f9107062746182a988e2e2798a767b5f1cb6c6
InTheRain <indrswara@gmail.com>
2023-09-26 19:38:34 +0700

    update multiple Linear

--- test/Test.java
+++ test/Test.java
@@ -1,15 +1,18 @@
+import Application.MultipleLinear;
 import Image.ImageLoader;
 import Image.ImageSaver;
 import Image.Resize;
 import Matrix.*;
 import Matrix.OBE.OBERunner;
+import Vector.EquationSpace;
 import Vector.EuclideanSpace;
 import Vector.VectorSpace;
 
 public class Test {
     public static void main(String[] args){
-        var image = ImageLoader.load("./test/debug.png");
-        var resized = Resize.resize(image, 100);
-        ImageSaver.save(resized, "./tmp/debug-res.png");
+        var newMat = MatrixReader.readCLI();
+        var result = MultipleLinear.solve(newMat);
+        MultipleLinear.Display(result);
+        
     }
 }
