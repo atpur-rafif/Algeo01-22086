969e67a3dbf32c6cdef2a7c22c6eac0eebadb9ad
InTheRain <indrswara@gmail.com>
2023-09-26 15:00:51 +0700

    add file

--- test/Test.java
+++ test/Test.java
@@ -6,11 +6,16 @@ import Matrix.OBE.OBERunner;
 import Vector.EuclideanSpace;
 import Vector.VectorSpace;
 import Menu.*;
+import java.util.Scanner;
 
 public class Test {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args){
-        MainMenu.InterfaceProgram();
-
+        
+        Matrix test = MatrixReader.readCLI(); 
+        System.out.print("Berikan nama file: ");
+        String fileName = scanner.next();
+        MatrixPrinter.printMatrixFile(test, fileName);
 
     }
 }
