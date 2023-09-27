d35dfa66359db88ae9c9289012c4ac3fc94fb14f
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-09-27 09:29:05 +0700

    Testing for multiple input

--- src/GUI/MainApp.java
+++ src/GUI/MainApp.java
@@ -2,15 +2,23 @@ package GUI;
 
 import javax.swing.*;
 
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
 public class MainApp{
     public static void Show(){
         var frame = new JFrame("Matrix");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         var matrix = new JInputMatrix();
-        frame.getContentPane().add(matrix);
+        var matrix2 = new JInputMatrix();
 
-        frame.setSize(400, 300);
+        var panel = frame.getContentPane();
+        panel.setLayout(new GridLayout(2, 1));
+        panel.add(matrix);
+        panel.add(matrix2);
+
+        frame.setMinimumSize(new Dimension(400, 600));
         //frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
